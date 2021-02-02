f8c432806f0f30ba2e0a2117c1afdaf63aed9b5a
lacap-PC\lacap <paulsamuel77@gmail.com>
2021-02-03 00:26:21 +0800

    Stage 0 done 2

---
+++ Assets/Scripts/DragAroundTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAroundTarget : MonoBehaviour
+{
+    public bool isObjectInCorrectPosition { get; set; } = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+}
---
+++ Assets/Scripts/UI/StartButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    public GameObject Target;
+    DragAroundTarget[] _dragAroundTargets;
+    bool _isObjectsInCorrectPosition = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _dragAroundTargets = Target.GetComponentsInChildren<DragAroundTarget>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickStartButton()
+    {
+        _isObjectsInCorrectPosition = true;
+
+        foreach (DragAroundTarget dr in _dragAroundTargets)
+        {
+            _isObjectsInCorrectPosition = _isObjectsInCorrectPosition && dr.isObjectInCorrectPosition;
+        }
+
+        Debug.Log(_isObjectsInCorrectPosition);
+    }
+}
